7610965d8b69677cd913a9393883d6d601977b50
YUSRIN20 <massyusrin123@gmail.com>
2024-03-04 13:09:49 +0530

    added timestamp

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import  express  from "express";
-
+import fs from 'fs'
+import { format } from  'date-fns';
 const app  = express()
 const PORT =  4000;
 
@@ -10,6 +11,14 @@ app.get('/get-data',(req,res)=>{
     res.status(200) .json({message:"status code sucess",data:{name:"yusrin"}})
  })
  
+ app.get('/write-read',(req,res)=>{
+   let today = format(new Date(),'dd-mm-yyyy-HH-mm-ss')
+   console.log(('today',today))
+   const filepath = `TimeStamp/${today}.txt`
+   fs.writeFileSync(filepath,`${today}`,'utf8')
+   let data = fs.readFileSync(filepath,'utf8')
+   res.status(200).send(data)
+ })
 app.listen(PORT,()=>{
      console.log(`app is runningin thr port ${PORT}`);
 }) //running
